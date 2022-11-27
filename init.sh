e1f621a96b4c2c10f41799b07c44861d9be62125
Ezekiel Warren <zekewarren@gmail.com>
2022-11-27 14:18:23 -0800

    gh cli and bazel download

--- init.sh
+++ init.sh
@@ -18,6 +18,20 @@ if [ ! -d ".git" ]; then
   git clone https://github.com/zaucy/init.git .
 fi
 
+if [ -z "$(which gh)" ]; then
+  type -p curl >/dev/null || sudo apt install curl -y
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+  && sudo apt update \
+  && sudo apt install gh -y
+fi
+
+if [ -z "$(which bazel)" ]; then
+  gh release download -R bazelbuild/bazelisk -p 'bazelisk-linux-amd64' -O "$HOME/.local/bin/bazel" --clobber
+  chmod +x "$HOME/.local/bin/bazel"
+fi
+
 if [ -z "$(which cargo)" ]; then
   curl https://sh.rustup.rs -sSf | sh
 fi
