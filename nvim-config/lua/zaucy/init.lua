468d7e828c2ce0fa7ba9f945c7af4e7017c42e32
Ezekiel Warren <zekewarren@gmail.com>
2022-12-04 17:18:33 -0800

    neovide config

--- nvim-config/lua/zaucy/init.lua
+++ nvim-config/lua/zaucy/init.lua
@@ -3,3 +3,7 @@ require("zaucy.packer")
 require("zaucy.mason")
 require("zaucy.remap")
 
+if vim.g.neovide then
+	require("zaucy.neovide")
+end
+
---
+++ nvim-config/lua/zaucy/neovide.lua
@@ -0,0 +1,5 @@
+vim.g.neovide_cursor_animation_length = 0.005
+vim.g.neovide_refresh_rate = 144
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
+vim.g.neovide_transparency = 0.98
+
