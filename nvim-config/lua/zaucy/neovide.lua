99bea35bf65db4a527153462eb69c1b47fc32b5b
Ezekiel Warren <zekewarren@gmail.com>
2022-12-04 22:30:43 -0800

    neovide config

--- nvim-config/lua/zaucy/neovide.lua
+++ nvim-config/lua/zaucy/neovide.lua
@@ -1,5 +1,6 @@
-vim.g.neovide_cursor_animation_length = 0.005
+vim.g.neovide_cursor_animation_length = 0.02
 vim.g.neovide_refresh_rate = 144
 vim.g.neovide_cursor_vfx_mode = "pixiedust"
 vim.g.neovide_transparency = 0.98
+vim.opt.guifont = { "ProggyVector", ":h12" }
 
--- nvim-config/lua/zaucy/set.lua
+++ nvim-config/lua/zaucy/set.lua
@@ -1,6 +1,14 @@
+vim.g.mapleader = " "
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.smartindent = true
 vim.opt.cursorline = true
-vim.g.mapleader = " "
+vim.opt.list = true
+vim.opt.tabstop = 2
+vim.opt.listchars = {
+	lead = '.',
+	trail = '.',
+	tab = '🡢 ',
+}
 
