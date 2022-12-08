c78b438862e37e81c4427b7889347c12403494af
Ezekiel Warren <zekewarren@gmail.com>
2022-12-08 08:53:17 -0800

    neovide, zoxide in ubuntu

--- init.sh
+++ init.sh
@@ -55,3 +55,14 @@ if [ -z "$(which zoxide)" ]; then
   cargo install zoxide
 fi
 
+if [ -z "$(which neovide)" ]; then
+  sudo apt install -y curl \
+    gnupg ca-certificates git \
+    gcc-multilib g++-multilib cmake libssl-dev pkg-config \
+    libfreetype6-dev libasound2-dev libexpat1-dev libxcb-composite0-dev \
+    libbz2-dev libsndio-dev freeglut3-dev libxmu-dev libxi-dev libfontconfig1-dev \
+    libxcursor-dev
+
+  cargo install --git https://github.com/neovide/neovide
+fi
+
