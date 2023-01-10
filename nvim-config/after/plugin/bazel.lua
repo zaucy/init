3a26bf6298d2b53db5df3dc17d7788602ae63915
Ezekiel Warren <zekewarren@gmail.com>
2023-01-09 23:31:10 -0800

    bazel auto format

--- nvim-config/after/plugin/bazel.lua
+++ nvim-config/after/plugin/bazel.lua
@@ -13,3 +13,19 @@ for _, mode in ipairs({ "n", "i" }) do
 	)
 end
 
+local M = {}
+
+function M.buildifier()
+end
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.bazel", "*.bzl", "WORKSPACE", "BUILD" },
+	-- command = "%!buildifier",
+	callback = function(data)
+		local cursor_pos = vim.api.nvim_win_get_cursor(0)
+		vim.api.nvim_command("%!buildifier")
+		vim.api.nvim_win_set_cursor(0, cursor_pos)
+	end,
+})
+
+return M
