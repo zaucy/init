7f0f517b292e8a3c9a79810ba7fb4267404b79db
Ezekiel Warren <zekewarren@gmail.com>
2023-01-13 18:30:28 -0800

    remove bazel module config

--- nvim-config/after/plugin/bazel.lua
+++ nvim-config/after/plugin/bazel.lua
@@ -1,18 +1,3 @@
-local bazel = require('bazel')
-
-for _, mode in ipairs({ "n", "i" }) do
-	vim.keymap.set(
-		mode,
-		"<C-S-D>",
-		function()
-			bazel.select_target({}, function(target)
-				print(target)
-			end)
-		end,
-		{ noremap = true, expr = true }
-	)
-end
-
 local M = {}
 
 function M.buildifier()
