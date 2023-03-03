0d26d3c97196cbfdf5bbd73882293c4208851a7a
Ezekiel Warren <zekewarren@gmail.com>
2023-03-02 18:37:48 -0800

    toggleterm keys

--- nvim-config/lua/zaucy/remap.lua
+++ nvim-config/lua/zaucy/remap.lua
@@ -6,9 +6,50 @@ nnoremap("n", "nzzzv")
 nnoremap("N", "Nzzzv")
 nnoremap("<C-S-e>", ":NvimTreeFindFile<CR>")
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
-	"<C-\\>",
-	":ToggleTerm<CR>",
-	{ noremap = true }
+	"<M-1>",
+	function()
+		require('toggleterm').toggle_command(nil, 1)
+	end
+)
+
+vim.keymap.set(
+	"t",
+	"<M-1>",
+	function()
+		require('toggleterm').toggle_command(nil, 1)
+	end
+)
+
+vim.keymap.set(
+	"n",
+	"<M-2>",
+	function()
+		require('toggleterm').toggle_command(nil, 2)
+	end
+)
+
+vim.keymap.set(
+	"t",
+	"<M-2>",
+	function()
+		require('toggleterm').toggle_command(nil, 2)
+	end
+)
+
+vim.keymap.set(
+	"n",
+	"<M-3>",
+	function()
+		require('toggleterm').toggle_command(nil, 3)
+	end
+)
+
+vim.keymap.set(
+	"t",
+	"<M-3>",
+	function()
+		require('toggleterm').toggle_command(nil, 3)
+	end
 )
