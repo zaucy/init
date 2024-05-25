e1aa9c67576cd4482b9b523d50e17097566a5548
Ezekiel Warren <zekewarren@gmail.com>
2024-05-25 12:59:05 -0700

    chore: more

--- nvim-config/lua/plugins/lsp.lua
+++ nvim-config/lua/plugins/lsp.lua
@@ -6,8 +6,18 @@ return {
         nushell = {},
         gdscript = {},
         starpls = {},
+        omnisharp = { mason = false }, -- using roslyn instead
       },
       inlay_hints = { enabled = false },
+    },
+  },
+  {
+    "jmederosalvarado/roslyn.nvim",
+    cmds = { "CSInstallRosylyn", "CSTarget" },
+    filetypes = { "cs" },
+    opts = {
+      on_attach = function() end,
+      capabilities = nil,
     },
   },
 }
