d6355dfa88a5667413d13142e6c0d0fdaf6d5473
Ezekiel Warren <zekewarren@gmail.com>
2024-06-25 10:55:10 -0700

    feat: minimal config oil tweak

--- nvim-config-minimal/lua/plugins/filetree.lua
+++ nvim-config-minimal/lua/plugins/filetree.lua
@@ -1,7 +1,18 @@
 return {
 	{
 		"stevearc/oil.nvim",
-		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			default_file_explorer = true,
+			skip_confirm_for_simple_edits = true,
+			columns = {
+				"icon",
+				"permissions",
+			},
+			view_options = {
+				show_hidden = false,
+			},
+		},
 		cmd = { "Oil" },
 		keys = {
 			{ "<leader>e", "<cmd>Oil<cr>",   desc = "Explore Files" },
