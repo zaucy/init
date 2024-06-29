5ffdffef281b76d0465082136ffb7366548c56ca
Ezekiel Warren <zekewarren@gmail.com>
2024-06-29 16:57:20 -0700

    feat: lsp virtual text at wrap line

--- nvim-config-minimal/lua/config/options.lua
+++ nvim-config-minimal/lua/config/options.lua
@@ -22,7 +22,8 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 	vim.lsp.diagnostic.on_publish_diagnostics, {
 		underline = true,
 		virtual_text = {
-			spacing = 4,
+			spacing = 1,
+			virt_text_win_col = 80,
 		},
 		signs = false,
 		update_in_insert = false,
