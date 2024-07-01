0d7f885a2094b3dc7f3d090f26fead22c6193858
Ezekiel Warren <zekewarren@gmail.com>
2024-07-01 12:53:02 -0700

    feat: new git related keys

--- nvim-config-minimal/lua/plugins/git.lua
+++ nvim-config-minimal/lua/plugins/git.lua
@@ -13,6 +13,8 @@ return {
 			{ "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle Git Blame" },
 			{ "]h",         "<cmd>Gitsigns next_hunk<cr>",                 desc = "Next Git Hunk" },
 			{ "[h",         "<cmd>Gitsigns prev_hunk<cr>",                 desc = "Previous Git Hunk" },
+			{ "<leader>ga", "<cmd>Gitsigns stage_buffer<cr>",              desc = "Stage Current Buffer" },
+			{ "<leader>gd", "<cmd>Gitsigns diffthis<cr>",                  desc = "View Buffer Diff" },
 		},
 	}
 }
