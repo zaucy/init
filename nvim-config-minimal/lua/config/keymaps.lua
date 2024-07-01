c6ca362ebdffeaf446e5afb74234da75d58234d7
Ezekiel Warren <zekewarren@gmail.com>
2024-07-01 12:52:19 -0700

    feat: better terminal toggle

--- nvim-config-minimal/lua/config/keymaps.lua
+++ nvim-config-minimal/lua/config/keymaps.lua
@@ -16,12 +16,77 @@ vim.keymap.set({ "n" }, "gbm", goto_closest_file("MODULE.bazel"), { desc = "Baze
 vim.keymap.set({ "n" }, "gbw", goto_closest_file("WORKSPACE.bazel"), { desc = "Bazel Workspace File" })
 vim.keymap.set({ "n" }, "gbz", goto_closest_file(".bazelrc"), { desc = "Bazelrc File" })
 
-vim.keymap.set({ "n" }, "<C-_>", "<cmd>terminal<cr>", { desc = "Open Terminal" })
-vim.keymap.set({ "n" }, "<C-/>", "<cmd>terminal<cr>", { desc = "Open Terminal" })
+local function is_bufvalid(buf)
+	if buf == nil then return false end
+	if not vim.api.nvim_buf_is_valid(buf) then return false end
+	local buftype = vim.bo[buf].buftype
+	if buftype == "prompt" then return false end
+	if buftype == "nofile" then return false end
+	print(buftype)
+	return true
+end
+
+local function find_nearby_valid_buf(bufs, start_index)
+	local offset = 1
+	while true do
+		if start_index + offset > #bufs and start_index - offset < 0 then
+			return nil
+		end
+
+		if is_bufvalid(bufs[start_index + offset]) then
+			return bufs[start_index + offset]
+		elseif is_bufvalid(bufs[start_index - offset]) then
+			return bufs[start_index - offset]
+		end
+
+		offset = offset + 1
+	end
+end
+
+local function close_terminal()
+	if vim.bo.buftype ~= "terminal" then
+		return
+	end
+
+	local currbuf = vim.fn.winbufnr(0)
+	local allbufs = vim.api.nvim_list_bufs()
+	for i, buf in ipairs(allbufs) do
+		if buf == currbuf then
+			local nearby_buf = find_nearby_valid_buf(allbufs, i)
+			if nearby_buf ~= nil then
+				vim.api.nvim_set_current_buf(nearby_buf)
+			else
+				vim.cmd.close()
+			end
+			return
+		end
+	end
+
+	vim.cmd.close()
+end
+
+local function open_terminal()
+	if vim.bo.buftype == "terminal" then
+		close_terminal()
+		return
+	end
+
+	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.bo[buf].buftype == "terminal" then
+			vim.api.nvim_set_current_buf(buf)
+			return
+		end
+	end
+
+	vim.cmd("terminal")
+end
+
+vim.keymap.set({ "n" }, "<C-_>", open_terminal, { desc = "Open Terminal" })
+vim.keymap.set({ "n" }, "<C-/>", open_terminal, { desc = "Open Terminal" })
 vim.keymap.set({ "t" }, "<esc><esc>", "<C-\\><C-n>", { desc = "Enter Normal Mode" })
 vim.keymap.set({ "t" }, "<C-w>", "<C-\\><C-n><cmd>WhichKey <C-w><cr>", {})
-vim.keymap.set({ "t" }, "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
-vim.keymap.set({ "t" }, "<C-_>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+vim.keymap.set({ "t" }, "<C-/>", close_terminal, { desc = "Hide Terminal" })
+vim.keymap.set({ "t" }, "<C-_>", close_terminal, { desc = "Hide Terminal" })
 
 -- move lines
 vim.keymap.set({ "n" }, "<a-j>", "<cmd>m .+1<cr>==", { desc = "move down" })
