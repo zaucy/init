607142ab62a78f66435e83831f0de16215c6a707
Ezekiel Warren <zekewarren@gmail.com>
2024-07-27 10:03:35 -0700

    feat: respect gitignore for oil ignore

--- nvim-config-minimal/lua/plugins/oil.lua
+++ nvim-config-minimal/lua/plugins/oil.lua
@@ -1,3 +1,41 @@
+local git_ignored = setmetatable({}, {
+	__index = function(self, key)
+		local proc = vim.system(
+			{ "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+			{
+				cwd = key,
+				text = true,
+			}
+		)
+		local result = proc:wait()
+		local ret = {}
+		if result.code == 0 then
+			for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+				-- Remove trailing slash
+				line = line:gsub("/$", "")
+				table.insert(ret, line)
+			end
+		end
+
+		rawset(self, key, ret)
+		return ret
+	end,
+})
+
+local function is_hidden_file(name, _)
+	-- dotfiles are always considered hidden
+	if vim.startswith(name, ".") then
+		return true
+	end
+	local dir = require("oil").get_current_dir()
+	-- if no local directory (e.g. for ssh connections), always show
+	if not dir then
+		return false
+	end
+	-- Check if file is gitignored
+	return vim.list_contains(git_ignored[dir], name)
+end
+
 return {
 	{
 		"stevearc/oil.nvim",
@@ -18,6 +56,7 @@ return {
 			},
 			view_options = {
 				show_hidden = false,
+				is_hidden_file = is_hidden_file,
 			},
 		},
 		cmd = { "Oil" },
