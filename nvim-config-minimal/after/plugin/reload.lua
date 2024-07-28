43b8ccffdfa659355c6429f527280fd07f8c30e7
Ezekiel Warren <zekewarren@gmail.com>
2024-07-28 10:33:21 -0700

    feat: reload/update remember line and col

--- nvim-config-minimal/after/plugin/reload.lua
+++ nvim-config-minimal/after/plugin/reload.lua
@@ -1,28 +1,34 @@
-local function reopen_neovide_detached()
-	local current_file = vim.fn.expand("%:p")
-	local args = { "--", "+ReloadDone " .. tostring(vim.uv.os_getppid()) }
-
-	if current_file then
-		table.insert(args, "--")
-		table.insert(args, current_file)
+local function make_reopen_neovide_detached_fn()
+	if not vim.g.neovide then
+		return function() end
 	end
 
-	print(vim.inspect(args))
+	local current_file = vim.fs.normalize(vim.fn.expand("%:p"))
+	local reload_done_cmd = "+ReloadDone " .. tostring(vim.uv.os_getppid())
+	local files = {}
 
-	local handle = vim.uv.spawn("neovide", {
-		cwd = vim.fn.getcwd(),
-		args = args,
-		detached = true,
-		hide = true,
-	})
+	if current_file then
+		local _, line, col = unpack(vim.fn.getcursorcharpos())
+		table.insert(files, { current_file, line, col })
+	end
 
-	---@diagnostic disable-next-line: need-check-nil
-	handle:unref()
+	reload_done_cmd = reload_done_cmd .. " " .. vim.json.encode(files)
+	return function()
+		local handle = vim.uv.spawn("neovide", {
+			cwd = vim.fn.getcwd(),
+			args = { "--", reload_done_cmd },
+			detached = true,
+			hide = true,
+		})
+
+		---@diagnostic disable-next-line: need-check-nil
+		handle:unref()
+	end
 end
 
 local function reload_command()
 	if vim.g.neovide then
-		reopen_neovide_detached()
+		make_reopen_neovide_detached_fn()()
 	end
 end
 
@@ -35,21 +41,34 @@ local function reload_done_command(opts)
 
 	---@diagnostic disable-next-line: param-type-mismatch
 	vim.uv.kill(reload_pid, "sigkill")
+
+	for _, entry in ipairs(vim.json.decode(opts.fargs[2])) do
+		local file, line, col = unpack(entry)
+		vim.cmd("e " .. file)
+		vim.api.nvim_win_set_cursor(0, { line, col - 1 })
+	end
 end
 
 local function update_command()
-	vim.api.nvim_create_autocmd("User", {
-		pattern = "LazySync",
-		callback = function()
-			vim.cmd("Reload")
-		end,
-	})
+	local reload = make_reopen_neovide_detached_fn()
+	vim.uv.spawn("git", {
+		cwd = vim.fs.normalize("~/projects/zaucy/init"),
+		args = { "pull" },
+		hide = true,
+	}, vim.schedule_wrap(function(_, _)
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "LazySync",
+			callback = function()
+				reload()
+			end,
+		})
 
-	vim.cmd("Lazy sync")
+		vim.cmd("Lazy sync")
+	end))
 end
 
 if not vim.g.vscode then
 	vim.api.nvim_create_user_command("Reload", reload_command, {})
-	vim.api.nvim_create_user_command("ReloadDone", reload_done_command, { nargs = 1 })
+	vim.api.nvim_create_user_command("ReloadDone", reload_done_command, { nargs = '*' })
 	vim.api.nvim_create_user_command("Update", update_command, {})
 end
