36825bdbddccb5b4f1a334d93df050b44ea8fb0d
Ezekiel Warren <zekewarren@gmail.com>
2024-08-17 09:27:00 -0700

    feat: bring back notify

---
+++ nvim-config-minimal/lua/plugins/notify.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		"rcarriga/nvim-notify",
+		lazy = false,
+		config = function()
+			---@diagnostic disable-next-line: missing-fields
+			require("notify").setup({
+				fps = 120,
+				on_open = function(win)
+					vim.api.nvim_win_set_config(win, { focusable = false })
+				end,
+			})
+			vim.notify = require("notify")
+			vim.keymap.set(
+				"n", "<Esc>",
+				function() require("notify").dismiss({ pending = false, silent = true }) end,
+				{ desc = "dismiss notify popup and clear hlsearch" }
+			)
+		end,
+		keys = {
+			{ "<leader>n", "<cmd>Telescope notify theme=ivy<cr>" },
+		},
+	}
+}
