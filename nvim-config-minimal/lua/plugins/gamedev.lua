03e4f4eafad6545becd948a7a7954604d769f9e9
Ezekiel Warren <ezekiel@seaube.com>
2024-08-19 08:07:11 -0700

    feat: new gamedev unreal keys

--- nvim-config-minimal/lua/plugins/gamedev.lua
+++ nvim-config-minimal/lua/plugins/gamedev.lua
@@ -6,11 +6,12 @@ return {
 		lazy = false,
 		opts = {},
 		keys = {
-			{ "<leader>uo", "<cmd>Uproject open<cr>",                                                          desc = "Open Unreal Editor" },
-			{ "<leader>ur", "<cmd>Uproject reload show_output<cr>",                                            desc = "Reload uproject" },
-			{ "<leader>up", "<cmd>Uproject play<cr>",                                                          desc = "Play game" },
-			{ "<leader>ub", "<cmd>Uproject build ignore_junk close_output_on_success type_pattern=Editor<cr>", desc = "Build" },
-			{ "<leader>uB", "<cmd>Uproject build type_pattern=Editor<cr>",                                     desc = "Build (keep open)" },
+			{ "<leader>uo", "<cmd>Uproject open<cr>",                                                               desc = "Open Unreal Editor" },
+			{ "<leader>ur", "<cmd>Uproject reload show_output<cr>",                                                 desc = "Reload uproject" },
+			{ "<leader>up", "<cmd>Uproject play log_cmds=Log\\ Log<cr>",                                            desc = "Play game" },
+			{ "<leader>ub", "<cmd>Uproject build ignore_junk close_output_on_success type_pattern=Editor wait<cr>", desc = "Build" },
+			{ "<leader>uB", "<cmd>Uproject build type_pattern=Editor wait<cr>",                                     desc = "Build (keep open)" },
+			{ "<leader>uc", "<cmd>Uproject build_plugins type_pattern=Editor wait<cr>",                             desc = "Build Plugins" },
 		},
 	},
 }
