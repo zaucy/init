570e1772fddd4eea4a721e3c2c012931a3c88e67
Ezekiel Warren <zekewarren@gmail.com>
2024-08-31 13:59:19 -0700

    feat: markdown render plugin

---
+++ nvim-config-minimal/lua/plugins/markdown.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {
+			debounce = 0,
+			sign = { enabled = false },
+			anti_conceal = {
+				enabled = true,
+			},
+			heading = {
+				position = 'inline',
+				width = 'block',
+				icons = { '', '', '', '', '', '' },
+				border = true,
+				above = ' ',
+				below = '▔',
+				left_pad = 0,
+				right_pad = 2,
+				backgrounds = {
+					"Transparent",
+					"Transparent",
+					"Transparent",
+					"Transparent",
+					"Transparent",
+					"Transparent",
+				},
+			},
+			code = {
+				below = '▔',
+			},
+		},
+	}
+}
