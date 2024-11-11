150f79fefb082b95d5b0911ad57d322bd3a6bba3
Ezekiel Warren <ezekiel@seaube.com>
2024-11-11 09:10:48 -0800

    feat: proj watches for dir changes now

--- nvim-config-minimal/lua/plugins/proj.lua
+++ nvim-config-minimal/lua/plugins/proj.lua
@@ -1,11 +1,14 @@
 return {
 	"zaucy/proj.nvim",
+	dir = "~/projects/zaucy/proj.nvim",
 	dependencies = {
 		'nvim-lua/plenary.nvim',
 		'nvim-telescope/telescope.nvim',
 		'MunifTanjim/nui.nvim',
 	},
-	opts = {},
+	opts = {
+		hook_dir_changed = true,
+	},
 	cmd = { "ProjInfo" },
 	keys = {
 		{ "<leader>p", "<cmd>Telescope proj theme=ivy layout_strategy=horizontal<cr>", desc = "Open Project" },
