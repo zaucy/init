1a4aab799a74dd2a8e46c94f434953c8a17d57b7
Ezekiel Warren <zekewarren@gmail.com>
2024-12-01 16:45:10 -0800

    fiX: missing mode

--- nvim-config-minimal/lua/plugins/nos.lua
+++ nvim-config-minimal/lua/plugins/nos.lua
@@ -7,7 +7,7 @@ return {
 			local nos = require('nos')
 			nos.setup({})
 			vim.keymap.set({ 'n', "v" }, 'gs', nos.opkeymapfunc, { expr = true })
-			vim.keymap.set({ '' }, 'gss', nos.bufkeymapfunc)
+			vim.keymap.set({ 'n' }, 'gss', nos.bufkeymapfunc)
 		end,
 	}
 }
