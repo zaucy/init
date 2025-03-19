c25316cbf88854a6de1911254564252bbcc4bace
Ezekiel Warren <ezekiel@seaube.com>
2025-03-19 14:50:05 -0700

    feat: telescope backdrop

--- nvim-config-minimal/lua/plugins/telescope.lua
+++ nvim-config-minimal/lua/plugins/telescope.lua
@@ -39,6 +39,50 @@ local telescope_opts = {
 	}
 }
 
+local function setup_telescope_backdrop()
+	-- https://github.com/nvim-telescope/telescope.nvim/issues/3020
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "TelescopePrompt",
+		callback = function(ctx)
+			local backdropName = "TelescopeBackdrop"
+			local telescopeBufnr = ctx.buf
+
+			-- `Telescope` does not set a zindex, so it uses the default value
+			-- of `nvim_open_win`, which is 50: https://neovim.io/doc/user/api.html#nvim_open_win()
+			local telescopeZindex = 50
+
+			local backdropBufnr = vim.api.nvim_create_buf(false, true)
+			local winnr = vim.api.nvim_open_win(backdropBufnr, false, {
+				relative = "editor",
+				row = 0,
+				col = 0,
+				width = vim.o.columns,
+				height = vim.o.lines,
+				focusable = false,
+				style = "minimal",
+				zindex = telescopeZindex - 1, -- ensure it's below the reference window
+			})
+
+			vim.api.nvim_set_hl(0, backdropName, { bg = "#000000", default = true })
+			vim.wo[winnr].winhighlight = "Normal:" .. backdropName
+			vim.wo[winnr].winblend = 50
+			vim.bo[backdropBufnr].buftype = "nofile"
+
+			-- close backdrop when the reference buffer is closed
+			vim.api.nvim_create_autocmd({ "WinClosed", "BufLeave" }, {
+				once = true,
+				buffer = telescopeBufnr,
+				callback = function()
+					if vim.api.nvim_win_is_valid(winnr) then vim.api.nvim_win_close(winnr, true) end
+					if vim.api.nvim_buf_is_valid(backdropBufnr) then
+						vim.api.nvim_buf_delete(backdropBufnr, { force = true })
+					end
+				end,
+			})
+		end,
+	})
+end
+
 local function telescope_fzf_build_cmd()
 	if vim.fn.has('win32') == 1 then
 		return
@@ -57,6 +101,7 @@ return {
 		},
 		config = function()
 			require('telescope').setup(telescope_opts)
+			setup_telescope_backdrop()
 		end,
 		cmd = { "Telescope" },
 		keys = {
