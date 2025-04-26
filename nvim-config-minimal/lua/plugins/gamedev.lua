b50895fc4ccda4564f1c5ff3345a5a894a7f102a
Ezekiel Warren <zekewarren@gmail.com>
2025-04-26 15:11:28 -0700

    feat: crude unreal header search

--- nvim-config-minimal/lua/plugins/gamedev.lua
+++ nvim-config-minimal/lua/plugins/gamedev.lua
@@ -8,9 +8,159 @@ local function find_public_dirs(path)
 	})
 end
 
+--- @param filepath string
+--- @return boolean
+local function is_private_header(filepath)
+	local idx = string.find(filepath, "/Private/", 0, true)
+	return idx ~= nil
+end
+
+
+--- @param filepath string
+--- @return string
+local function get_header_module_name(filepath)
+	local prefixes = { "/Public/", "/Classes/" }
+	for _, prefix in ipairs(prefixes) do
+		local idx = string.find(filepath, prefix, 0, true)
+		if idx ~= nil then
+			local dir = string.sub(filepath, 0, idx - 1)
+			local dir_segments = vim.split(dir, "/", { plain = true })
+			local module_name = dir_segments[#dir_segments]
+			if module_name == "Source" then
+				module_name = dir_segments[#dir_segments - 1]
+			end
+			return module_name
+		end
+	end
+
+	local filepath_segments = vim.split(filepath, "/", { plain = true })
+	return filepath_segments[#filepath_segments - 1]
+end
+
+
+--- @param prefixes string[]
+--- @param filepath string
+--- @return string
+local function get_header_include_string(prefixes, filepath)
+	for _, prefix in ipairs(prefixes) do
+		local _, idx = string.find(filepath, prefix, 0, true)
+		if idx ~= nil then
+			return string.sub(filepath, idx + 1)
+		end
+	end
+
+	return filepath
+end
+
+local function telescope_unreal_headers()
+	require('uproject').get_project_engine_info(vim.fn.getcwd(), function(info)
+		if info == nil then
+			vim.notify("cannot find unreal project", vim.log.levels.ERROR)
+			return
+		end
+		local engine_dir = vim.fs.joinpath(info.install_dir, "Engine")
+		local source_dir = vim.fs.joinpath(engine_dir, "Source")
+		local plugins_dir = vim.fs.joinpath(engine_dir, "Plugins")
+		local project_source_dir = vim.fs.joinpath(info.project_dir, "Source")
+		local project_plugins_dir = vim.fs.joinpath(info.project_dir, "Plugins")
+		local entry_display = require("telescope.pickers.entry_display")
+		local finders = require("telescope.finders")
+		local make_entry = require("telescope.make_entry")
+		local pickers = require("telescope.pickers")
+		local sorters = require("telescope.sorters")
+		local previewers = require("telescope.previewers")
+		local themes = require("telescope.themes")
+		local actions = require("telescope.actions")
+		local action_state = require("telescope.actions.state")
+
+		local displayer = entry_display.create({
+			separator = " │ ",
+			items = {
+				{ width = 32 }, -- module name
+				{ remaining = true }, -- header path
+			},
+		})
+
+		--- @param entry {header_info: UnrealHeaderInfo}
+		local function entry_display_fn(entry)
+			return displayer({
+				entry.header_info.module_name,
+				entry.header_info.include_string,
+			})
+		end
+
+		local function custom_entry_maker(filepath)
+			filepath = string.gsub(filepath, "\\", "/")
+
+			local entry = make_entry.gen_from_file({})(filepath)
+			local private = is_private_header(filepath)
+			local module_name = get_header_module_name(filepath)
+			local include_string = get_header_include_string({ "/Public/", "/Classes/", "/Source/" }, filepath)
+			--- @type UnrealHeaderInfo
+			entry.header_info = {
+				private = private,
+				module_name = module_name,
+				include_string = include_string,
+			}
+			entry.value = filepath
+			entry.filename = filepath
+			entry.ordinal = module_name .. " " .. include_string
+			entry.display = entry_display_fn
+			return entry
+		end
+
+		local find_command = {
+			"fd",
+			"--glob", "**/*.h",
+			"-t", "f", -- files only
+			"-E", "*.generated.h",
+			"-E", "**/Thirdparty/*",
+			"-E", "**/ThirdParty/*",
+			"-E", "**/Binaries/*",
+			"-E", "**/Private/*",
+			"-E", "**/Internal/*",
+			"-E", "**/Intermediate/*",
+			"--search-path", source_dir,
+			"--search-path", plugins_dir,
+			"--search-path", project_source_dir,
+			"--search-path", project_plugins_dir,
+		}
+
+		table.insert(find_command, "--")
+
+		pickers.new(themes.get_ivy({}), {
+			prompt_title = "Unreal Headers",
+			finder = finders.new_oneshot_job(find_command, {
+				entry_maker = custom_entry_maker,
+				cwd = info.project_dir,
+			}),
+			previewer = previewers.vim_buffer_cat.new({}),
+			sorter = sorters.get_fuzzy_file(),
+			attach_mappings = function(prompt_bufnr, map)
+				map('i', '<C-y>', function()
+					--- @type {filename: string, header_info: UnrealHeaderInfo}
+					local entry = action_state.get_selected_entry()
+					vim.fn.setreg("m", entry.header_info.module_name)
+					vim.fn.setreg("f", entry.filename)
+					vim.fn.setreg("i", "#include \"" .. entry.header_info.include_string .. "\"\n")
+					actions.close(prompt_bufnr)
+				end)
+
+				return true
+			end,
+		}):find()
+	end)
+end
+
+--- @class UnrealHeaderInfo
+--- @field private boolean
+--- @field module_name string
+--- @field include_string string
+
 return {
 	{
 		"zaucy/uproject.nvim",
+		dir = "~/projects/zaucy/uproject.nvim",
 		dependencies = {
 			'nvim-lua/plenary.nvim',
 			"j-hui/fidget.nvim", -- optional
@@ -45,90 +195,7 @@ return {
 
 			{
 				"<leader>uh",
-				function()
-					require('uproject').get_project_engine_info(vim.fn.getcwd(), function(info)
-						if info == nil then
-							vim.notify("cannot find unreal project", vim.log.levels.ERROR)
-						end
-						local engine_dir = vim.fs.joinpath(info.install_dir, "Engine")
-						local source_dir = vim.fs.joinpath(engine_dir, "Source")
-						local plugins_dir = vim.fs.joinpath(engine_dir, "Plugins")
-						local builtin = require('telescope.builtin')
-						local entry_display = require("telescope.pickers.entry_display")
-						local finders = require("telescope.finders")
-						local make_entry = require("telescope.make_entry")
-						local pickers = require("telescope.pickers")
-						local sorters = require("telescope.sorters")
-
-						local displayer = entry_display.create({
-							separator = " │ ",
-							items = {
-								{ width = 2 }, -- icon
-								{ width = 40 }, -- module name
-								{ remaining = true }, -- header path
-							},
-						})
-
-						local function custom_entry_maker(filepath)
-							local entry = make_entry.gen_from_file({})(filepath)
-							entry.value = filepath
-							entry.filename = filepath
-							entry.display = function(entry_inner)
-								return displayer({
-									"󰦱",
-									"<module-name>",
-									entry_inner.value,
-								})
-							end
-							return entry
-						end
-
-						-- local public_dirs = vim.list_extend(
-						-- 	find_public_dirs(source_dir),
-						-- 	find_public_dirs(plugins_dir)
-						-- )
-
-						local find_command = {
-							"fd",
-							"--glob", "**/*.h",
-							"-t", "f", -- files only
-							"-E", "*.generated.h",
-							"-E", "*/Thirdparty/*",
-							"-E", "*/ThirdParty/*",
-						}
-
-						-- for _, dir in ipairs(public_dirs) do
-						-- 	table.insert(find_command, "--search-path")
-						-- 	table.insert(find_command, dir)
-						-- end
-
-						table.insert(find_command, "--")
-
-						pickers.new({}, {
-							prompt_title = "Unreal Headers",
-							finder = finders.new_oneshot_job(find_command, {
-								entry_maker = custom_entry_maker,
-								cwd = engine_dir,
-							}),
-							-- sorter = sorters.get_fuzzy_file(),
-							sorter = sorters.get_fzy_sorter(),
-						}):find()
-
-						-- builtin.find_files({
-						-- 	cwd = engine_dir,
-						-- 	find_command = {
-						-- 		"fd",
-						-- 		"--glob", "**/*.h",
-						-- 		"-t", "f", -- files only
-						-- 		"-E", "*.generated.h",
-						-- 		"-E", "*/Thirdparty/*",
-						-- 		"-E", "*/ThirdParty/*",
-						-- 		"--"
-						-- 	},
-						-- 	search_dirs = public_dirs,
-						-- })
-					end)
-				end,
+				function() telescope_unreal_headers() end,
 				desc = "Find unreal headers",
 			},
 		},
