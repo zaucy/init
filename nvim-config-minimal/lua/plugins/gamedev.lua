8e69a8eeccac3be2dee7ba1e7f2cba309dd6b117
Ezekiel Warren <zekewarren@gmail.com>
2025-05-13 21:26:11 -0700

    feat: clean and debug for uproject gamedev

--- nvim-config-minimal/lua/plugins/gamedev.lua
+++ nvim-config-minimal/lua/plugins/gamedev.lua
@@ -168,14 +168,15 @@ return {
 		cmd = { "Uproject" },
 		opts = {},
 		keys = {
-			{ "<leader>uu", "<cmd>Uproject show_output<cr>",                            desc = "Show last output" },
-			{ "<leader>uo", "<cmd>Uproject open<cr>",                                   desc = "Open Unreal Editor" },
-			{ "<leader>uO", "<cmd>Uproject build  type_pattern=Editor wait open<cr>",   desc = "Build and open Unreal Editor" },
-			{ "<leader>ur", "<cmd>Uproject reload show_output<cr>",                     desc = "Reload uproject" },
-			{ "<leader>up", "<cmd>Uproject play log_cmds=Log\\ Log<cr>",                desc = "Play game" },
-			{ "<leader>uP", "<cmd>Uproject play debug log_cmds=Log\\ Log<cr>",          desc = "Play game (debug)" },
-			{ "<leader>uB", "<cmd>Uproject build type_pattern=Editor wait<cr>",         desc = "Build" },
-			{ "<leader>uc", "<cmd>Uproject build_plugins type_pattern=Editor wait<cr>", desc = "Build Plugins" },
+			{ "<leader>uu",  "<cmd>Uproject show_output<cr>",                         desc = "Show last output" },
+			{ "<leader>uo",  "<cmd>Uproject open<cr>",                                desc = "Open Unreal Editor" },
+			{ "<leader>uO",  "<cmd>Uproject build type_pattern=Editor wait open<cr>", desc = "Build and open Unreal Editor" },
+			{ "<leader>ur",  "<cmd>Uproject reload show_output<cr>",                  desc = "Reload uproject" },
+			{ "<leader>up",  "<cmd>Uproject play log_cmds=Log\\ Log<cr>",             desc = "Play game" },
+			{ "<leader>uB",  "<cmd>Uproject build type_pattern=Editor wait<cr>",      desc = "Build" },
+			{ "<leader>uc",  "<cmd>Uproject clean type_pattern=Editor<cr>",           desc = "Clean" },
+			{ "<leader>udo", "<cmd>Uproject open debug<cr>",                          desc = "Open Unreal Editor (debug)" },
+			{ "<leader>udp", "<cmd>Uproject play debug<cr>",                          desc = "Play game (debug)" },
 
 			{
 				"<leader>ub",
