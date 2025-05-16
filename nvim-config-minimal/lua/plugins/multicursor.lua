c43985b38b979182a6d0fa8f9c18af5967c668fb
Ezekiel Warren <zekewarren@gmail.com>
2025-05-16 16:53:21 -0700

    feat: multicursor on select + some more multicursor keys

--- nvim-config-minimal/lua/plugins/multicursor.lua
+++ nvim-config-minimal/lua/plugins/multicursor.lua
@@ -14,12 +14,21 @@ return {
 			vim.keymap.set({ "n", "v" }, "gM", function() require("which-key").show({ keys = "gm", loop = true }) end,
 				{ desc = "Multicursor Hydra" })
 
+			vim.keymap.set({ "n", "v" }, "gmx", mc.deleteCursor, { desc = "Delete cursor" })
+			vim.keymap.set({ "n", "v" }, "gml", mc.nextCursor, { desc = "Next cursor" })
+			vim.keymap.set({ "n", "v" }, "gmh", mc.prevCursor, { desc = "Prev cursor" })
+			vim.keymap.set({ "n", "v" }, "gm<right>", mc.nextCursor, { desc = "Next cursor" })
+			vim.keymap.set({ "n", "v" }, "gm<left>", mc.prevCursor, { desc = "Prev cursor" })
+			vim.keymap.set({ "n", "v" }, "gm<c-l>", mc.lastCursor, { desc = "Last cursor" })
+			vim.keymap.set({ "n", "v" }, "gm<c-h>", mc.firstCursor, { desc = "First cursor" })
+			vim.keymap.set({ "n", "v" }, "gm<c-right>", mc.lastCursor, { desc = "Last cursor" })
+			vim.keymap.set({ "n", "v" }, "gm<c-left>", mc.firstCursor, { desc = "First cursor" })
+
 			vim.keymap.set({ "n", "v" }, "gm,", mc.clearCursors, { desc = "Clear Cursors" })
 			vim.keymap.set({ "n", "v" }, "gm<A-->", mc.deleteCursor, { desc = "Delete Cursor" })
 			vim.keymap.set({ "n", "v" }, "gm&", mc.alignCursors, { desc = "Align cursors" })
 			vim.keymap.set({ "n", "v" }, "gmm", mc.matchAllAddCursors, { desc = "Match all add cursor" })
 			vim.keymap.set({ "n", "v" }, "gm/", mc.searchAllAddCursors, { desc = "Search all add cursors" })
-			vim.keymap.set("v", "gms", mc.matchCursors, { desc = "Match search add cursor" })
 			vim.keymap.set({ "n", "v" }, "gmn", function() mc.matchAddCursor(1) end, { desc = "Match add cursor next" })
 			vim.keymap.set({ "n", "v" }, "gmN", function() mc.matchAddCursor(-1) end, { desc = "Match add cursor prev" })
 			vim.keymap.set({ "n", "v" }, "gm<up>", function() mc.lineAddCursor(-1) end,
@@ -58,5 +67,17 @@ return {
 			vim.keymap.set("n", "gmT", function() mc.addCursor("T" .. vim.fn.nr2char(vim.fn.getchar())) end,
 				{ desc = "Add cursor at before prev char" })
 		end
-	}
+	},
+	{
+		"zaucy/mcos.nvim",
+		dependencies = {
+			"jake-stewart/multicursor.nvim",
+		},
+		config = function()
+			local mcos = require('mcos')
+			mcos.setup({})
+			vim.keymap.set({ 'n', "v" }, 'gms', mcos.opkeymapfunc, { expr = true })
+			vim.keymap.set({ 'n' }, 'gmss', mcos.bufkeymapfunc)
+		end,
+	},
 }
