c161f8609d3742279c52c54f9ddc6707eb87191d
Ezekiel Warren <zekewarren@gmail.com>
2026-01-18 22:10:20 -0800

    chore: use local dir until these plugins work without local link

--- nvim-config-minimal/lua/plugins/gemini.lua
+++ nvim-config-minimal/lua/plugins/gemini.lua
@@ -30,7 +30,7 @@ end
 return {
 	{
 		"zaucy/mcp.nvim",
-		-- dir = "~/projects/zaucy/mcp.nvim",
+		dir = "~/projects/zaucy/mcp.nvim",
 		config = function()
 			local mcp = require("mcp")
 			mcp.setup({})
@@ -70,7 +70,7 @@ return {
 	},
 	{
 		"zaucy/gemini.nvim",
-		-- dir = "~/projects/zaucy/gemini.nvim",
+		dir = "~/projects/zaucy/gemini.nvim",
 		build = "bun install -g @google/gemini-cli@nightly",
 		dependencies = {
 			"zaucy/mcp.nvim",
