fe59c4d52216d6d26c17b06683a68809e2631a3e
Ezekiel Warren <zekewarren@gmail.com>
2026-02-16 17:29:39 -0800

    feat: user newer version of tree sitter

--- nvim-config-minimal/lua/plugins/treesitter.lua
+++ nvim-config-minimal/lua/plugins/treesitter.lua
@@ -3,20 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		lazy = false,
 		build = ":TSUpdate",
-		branch = "master",
-		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-				sync_install = false,
-				auto_install = true,
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-				indent = { enable = true },
-			})
-		end,
+		branch = "main",
 	},
 	{
 		branch = "main",
--- nvim-config-minimal/lua/zaucy/treesitter-parsers.lua
+++ nvim-config-minimal/lua/zaucy/treesitter-parsers.lua
@@ -1,50 +1,36 @@
 vim.api.nvim_create_autocmd("User", {
 	pattern = "TSUpdate",
 	callback = function()
-		require("nvim-treesitter.parsers").nu = {
-			install_info = {
-				url = "https://github.com/nushell/tree-sitter-nu.git",
-				files = { "src/parser.c" },
-				branch = "main",
-				requires_generate_from_grammar = false,
-				revision = "",
-			},
-			tier = 0,
-		}
-		---@diagnostic disable-next-line: inject-field
 		require("nvim-treesitter.parsers").bazelrc = {
 			install_info = {
-				url = "https://github.com/zaucy/tree-sitter-bazelrc.git",
+				url = "https://github.com/zaucy/tree-sitter-bazelrc",
 				files = { "src/parser.c" },
 				branch = "main",
 				requires_generate_from_grammar = false,
-				revision = "",
+				revision = nil,
 			},
 			tier = 0,
 		}
-		---@diagnostic disable-next-line: inject-field
 		require("nvim-treesitter.parsers").cpp2 = {
 			install_info = {
-				url = "https://github.com/tsoj/tree-sitter-cpp2.git",
+				url = "https://github.com/tsoj/tree-sitter-cpp2",
 				files = { "src/parser.c", "src/scanner.c" },
 				branch = "main",
 				generate_requires_npm = false,
 				requires_generate_from_grammar = false,
-				revision = "",
+				revision = nil,
 			},
 			tier = 0,
 		}
-		---@diagnostic disable-next-line: inject-field
 		require("nvim-treesitter.parsers").ecsact = {
 			install_info = {
-				url = "https://github.com/ecsact-dev/tree-sitter-ecsact.git",
+				url = "https://github.com/ecsact-dev/tree-sitter-ecsact",
 				files = { "src/parser.c" },
-				branch = "main",
 				generate_requires_npm = false,
 				requires_generate_from_grammar = false,
-				revision = "",
+				revision = nil,
 			},
-			tier = 0,
+			tier = 2,
 		}
 	end,
 })
